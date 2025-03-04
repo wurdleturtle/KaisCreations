071f8a82f75ce7f3a69f7537960961c747c487d4
unknown <wurdle@wurdle.eu>
2025-03-03 22:15:16 -0500

    commit 4

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -2,6 +2,16 @@ function Events() {
   return (
     <div className="centered">
       <h1> Events </h1>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non{" "}
+        <br />
+        dolorum possimus. Incidunt blanditiis excepturi sequi consequuntur{" "}
+        <br />
+        libero, explicabo magni eum exercitationem optio consequatur laborum
+        <br />
+        minima magnam neque temporibus reiciendis?
+      </p>
     </div>
   );
 }
--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -2,6 +2,16 @@ function Info() {
   return (
     <div className="centered">
       <h1> Info </h1>
+      <p>
+        {" "}
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda{" "}
+        <br />
+        aliquam quibusdam doloremque at quos sapiente a corrupti repellendus{" "}
+        <br />
+        quas iste, sint animi, dolorum rerum totam cumque. Earum mollitia quae{" "}
+        <br />
+        dicta!
+      </p>
     </div>
   );
 }
